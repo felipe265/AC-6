12dd0e479915ec1963d3390b0f0e7485c6dec0e7
Your Name <you@example.com>
2019-10-14 20:47:11 -0300

    criando a aplicação

---
+++ nao_entre_em_panico.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def nao_entre_em_panico():
+    for i in range(1, 550):
+        divisores = 0
+        for divisor in range(1, i+1):
+            if i % divisor == 0:
+                divisores += 1
+        if divisores == 2:
+            print(i, end=' ')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
